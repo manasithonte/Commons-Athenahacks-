a8b01ce6c15366ef8e1ec203207515811b43031d
manasithonte <manasithonte@gmail.com>
2026-03-10 21:19:28 -0700

    chore: fix missing frontend dependencies and cleanup backend warnings

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       dbName: "buddy_finder",
     });
     console.log("MongoDB Connected...");
